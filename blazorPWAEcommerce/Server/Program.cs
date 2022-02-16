b9ceb1297be2b5a082cf41eb6c3c4bcefb85e317
vivekreddybanka <56489987+vivekreddybanka@users.noreply.github.com>
2022-02-15 19:43:38 -0600

    Added the shared model product.

--- blazorPWAEcommerce/Server/Program.cs
+++ blazorPWAEcommerce/Server/Program.cs
@@ -1,3 +1,4 @@
+global using blazorPWAEcommerce.Shared;
 using Microsoft.AspNetCore.ResponseCompression;
 
 var builder = WebApplication.CreateBuilder(args);
---
+++ blazorPWAEcommerce/Shared/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blazorPWAEcommerce.Shared
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public decimal ProductPrice { get; set; }
+        public string ProductUrl { get; set; } = string.Empty;
+    }
+}
