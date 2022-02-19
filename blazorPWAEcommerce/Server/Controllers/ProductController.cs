82ed2e1799129d57609e306625e9840b4ac37cbc
vivekreddybanka <56489987+vivekreddybanka@users.noreply.github.com>
2022-02-19 14:21:02 -0600

    Added the swagger documentation to the project.

--- blazorPWAEcommerce/Server/Controllers/ProductController.cs
+++ blazorPWAEcommerce/Server/Controllers/ProductController.cs
@@ -36,7 +36,7 @@ namespace blazorPWAEcommerce.Server.Controllers
 
         [HttpGet]
 
-        public async Task<IActionResult> GetProduct()
+        public async Task<ActionResult<List<Product>>> GetProduct()
         {
             return  Ok(ProductListDetails);
         }
